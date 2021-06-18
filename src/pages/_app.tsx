80558cdfe65b3057f5c33446ca8d0bf398dd3f32
bfu4 <bella@sleeeepyti.me>
2021-06-18 02:44:26 -0400

    typescript it

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import "../styles/globals.css"
+import {AppProps} from "next/app";
 
-export default function App({Component, pageProps}): JSX.Element {
+export default function App({Component, pageProps}: AppProps): JSX.Element {
     return (
         <div>
             <Head>
